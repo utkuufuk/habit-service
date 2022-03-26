3f9d54e02c1d0297cc8ed6bd35536389a8d44242
Utku Ufuk <utkuufuk@gmail.com>
2022-03-26 21:37:35 +0300

    add debug log

--- internal/config/config.go
+++ internal/config/config.go
@@ -69,4 +69,5 @@ func init() {
 	GoogleClientSecret = os.Getenv("GSHEETS_CLIENT_SECRET")
 	GoogleAccessToken = os.Getenv("GSHEETS_ACCESS_TOKEN")
 	GoogleRefreshToken = os.Getenv("GSHEETS_REFRESH_TOKEN")
+	fmt.Println("PORT: ", HttpPort)
 }
