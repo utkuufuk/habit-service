e5201142ac14a15fe04f2f240b50235779efa3c7
Utku Ufuk <utkuufuk@gmail.com>
2022-04-03 19:16:26 +0300

    Reset task upon POST request from `entrello`  (#12)
    * parse labels & cell name from archived card
    
    * temp
    
    * get rid of all glados-related code & mark habit upon entrello request
    
    * add missing return statement

--- cmd/habit-service/main.go
+++ cmd/habit-service/main.go
@@ -7,9 +7,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/utkuufuk/habit-service/internal/config"
-	"github.com/utkuufuk/habit-service/internal/glados"
 	"github.com/utkuufuk/habit-service/internal/habit"
 	"github.com/utkuufuk/habit-service/internal/logger"
 	"github.com/utkuufuk/habit-service/internal/service"
@@ -28,74 +29,75 @@ func main() {
 	}
 
 	http.HandleFunc("/entrello", handleEntrelloRequest)
-	http.HandleFunc("/glados", handleGladosCommand)
 	http.ListenAndServe(fmt.Sprintf(":%d", config.HttpPort), nil)
 }
 
 func handleEntrelloRequest(w http.ResponseWriter, req *http.Request) {
-	// @fixme: make this its own handler, doing the same thing as what handleGladosCommand currently does
+	if req.Method == http.MethodGet {
+		action := service.FetchHabitsAsTrelloCardsAction{}
+		cards, err := action.Run(req.Context(), client)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, fmt.Sprintf("could not fetch new cards: %v", err))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cards)
+		return
+	}
+
 	if req.Method == http.MethodPost {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			logger.Error("Could not read request body: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		var card struct {
+			Desc   string `json:"desc"`
+			Labels []struct {
+				Name string `json:"name"`
+			} `json:"labels"`
+		}
+		if err = json.Unmarshal(body, &card); err != nil {
+			logger.Warn("Invalid request body: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		cell := strings.Split(card.Desc, "\n")[0]
+		matched, err := regexp.MatchString(`[a-zA-Z]{3} 202\d![A-Z][1-9][0-9]?$|^100$`, cell)
+		if err != nil || matched == false {
+			logger.Error("Invalid cell name '%s' in card description: %v", cell, err)
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+
+		symbol := "✔"
+		for _, c := range card.Labels {
+			if c.Name == "habit-skip" {
+				symbol = "–"
+				break
+			}
+			if c.Name == "habit-fail" {
+				symbol = "✘"
+				break
+			}
+		}
+
+		_, err = service.MarkHabitAction{Cell: cell, Symbol: symbol}.Run(req.Context(), client)
+		if err != nil {
+			logger.Error("Could not mark habit at cell '%s' as %s: %v", cell, symbol, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 		return
 	}
 
-	if req.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	action := service.FetchHabitsAsTrelloCardsAction{}
-	cards, err := action.Run(req.Context(), client)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, fmt.Sprintf("could not fetch new cards: %v", err))
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(cards)
-}
-
-func handleGladosCommand(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	type response struct {
-		Message string `json:"message"`
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response{fmt.Sprintf("Could not read HTTP request body: %v", err)})
-		return
-	}
-
-	var request struct {
-		Args []string `json:"args"`
-	}
-	if err = json.Unmarshal(body, &request); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response{fmt.Sprintf("Could not decode HTTP request body: %v", err)})
-		return
-	}
-
-	action, err := glados.ParseCommand(request.Args)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(response{fmt.Sprintf("Could not parse Glados command: %v", err)})
-		return
-	}
-
-	message, err := action.Run(req.Context(), client)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response{err.Error()})
-		return
-	}
-
-	json.NewEncoder(w).Encode(response{message})
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	return
 }
