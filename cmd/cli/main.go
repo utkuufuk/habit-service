92181e95cb3de29b380146a7bf8b63f2bbe701fc
Utku Ufuk <utkuufuk@gmail.com>
2022-05-16 22:17:08 +0300

    polish

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/utkuufuk/habit-service/internal/config"
 	"github.com/utkuufuk/habit-service/internal/logger"
@@ -21,35 +22,43 @@ func main() {
 	}
 
 	if len(os.Args) == 1 {
-		cards, err := service.FetchHabitCards(client, loc)
-		if err != nil {
-			logger.Error("Could not fetch undone habits: %v", err)
-			return
-		}
-		for _, card := range cards {
-			logger.Info("%s / %s / %s", card.Name, card.Desc, card.Due.Format("2006-01-02"))
-		}
+		displayCards(client, loc)
 		return
 	}
 
 	switch os.Args[1] {
 	case "progress-report":
-		cfg, err := config.ParseProgressReportConfig()
-		if err != nil {
-			logger.Error("Failed to parse server config: %v", err)
-			os.Exit(1)
-		}
-
-		if err = service.ReportProgress(
-			client,
-			cfg.TimezoneLocation,
-			cfg.SkipList,
-			cfg.TelegramChatId,
-			cfg.TelegramToken,
-		); err != nil {
-			logger.Error("Could not run send progress report: %v", err)
-		}
+		sendPorgressReport(client)
 	default:
-		logger.Error("Uncrecognized command: %s", os.Args[2])
+		logger.Error("Uncrecognized command: %s", os.Args[1])
+	}
+}
+
+func displayCards(client sheets.Client, loc *time.Location) {
+	cards, err := service.FetchHabitCards(client, loc)
+	if err != nil {
+		logger.Error("Could not fetch undone habits: %v", err)
+		return
+	}
+	for _, card := range cards {
+		logger.Info("%s / %s / %s", card.Name, card.Desc, card.Due.Format("2006-01-02"))
+	}
+}
+
+func sendPorgressReport(client sheets.Client) {
+	cfg, err := config.ParseProgressReportConfig()
+	if err != nil {
+		logger.Error("Failed to parse progress report config: %v", err)
+		os.Exit(1)
+	}
+
+	if err = service.ReportProgress(
+		client,
+		cfg.TimezoneLocation,
+		cfg.SkipList,
+		cfg.TelegramChatId,
+		cfg.TelegramToken,
+	); err != nil {
+		logger.Error("Could not run send progress report: %v", err)
 	}
 }
